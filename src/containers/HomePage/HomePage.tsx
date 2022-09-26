152642d2d2604392978fbbae27914dfc740c6229
BojanIstvancic <bojan.istvancic@gmail.com>
2022-09-26 22:47:19 +0200

    add mui, add axios, fetch data

--- src/containers/HomePage/HomePage.tsx
+++ src/containers/HomePage/HomePage.tsx
@@ -1,6 +1,26 @@
 import { RouteComponentProps } from "react-router-dom";
+import axios from "axios";
+import { useState, useEffect } from "react";
 
 const HomePage: React.FC<RouteComponentProps> = () => {
+  const [data, setData] = useState(null);
+
+  useEffect(() => {
+    getData();
+  }, []);
+
+  async function getData() {
+    try {
+      const response = await axios.get(
+        "https://jsonplaceholder.typicode.com/posts"
+      );
+      setData(response.data);
+    } catch (error) {
+      console.error(error);
+    }
+  }
+
+  console.log(data, "daturina");
   return (
     <>
       <p>HomePage</p>
