a309224e77a9f9d19c307a6e912c36fbd9532d33
BojanIstvancic <bojan.istvancic@gmail.com>
2022-09-26 22:32:07 +0200

    update app, create error page

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,22 @@
-import PublicRoutes from "./router";
+import { Route, Switch, BrowserRouter as Router } from "react-router-dom";
+import CreatePage from "./containers/CreatePage";
+import ErrorPage from "./containers/ErrorPage";
+import HomePage from "./containers/HomePage";
+import ItemDetailsPage from "./containers/ItemDetailsPage";
 
 export interface AppProps {}
 
 const App: React.FC<AppProps> = () => {
   return (
     <>
-      <PublicRoutes />
+      <Router>
+        <Switch>
+          <Route exact path="/" component={HomePage} />
+          <Route exact path="/create" component={CreatePage} />
+          <Route exact path="/details/:id" component={ItemDetailsPage} />
+          <Route component={ErrorPage} />
+        </Switch>
+      </Router>
     </>
   );
 };
