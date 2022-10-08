ad2b6065fead92b000f89e64765972e19f41a62a
BojanIstvancic <bojan.istvancic@gmail.com>
2022-10-08 13:16:13 +0200

    add routes as array

--- src/Routes.tsx
+++ src/Routes.tsx
@@ -6,24 +6,45 @@ import ItemDetailsPage from "./containers/ItemDetailsPage";
 import links from "./helpers/links";
 import { TransitionGroup, CSSTransition } from "react-transition-group";
 
-const Routes = () => {
+const RenderRoutes = () => {
   const location = useLocation();
+
+  const routes = [
+    {
+      path: links.create.pattern,
+      key: "create_page",
+      exact: true,
+      component: CreatePage,
+    },
+    {
+      path: links.itemDetails().pattern,
+      key: "item_details_page",
+      exact: true,
+      component: ItemDetailsPage,
+    },
+    {
+      path: links.home.pattern,
+      key: "home_page",
+      exact: true,
+      component: HomePage,
+    },
+    {
+      key: "error_page",
+      component: ErrorPage,
+    },
+  ];
+
   return (
     <TransitionGroup>
       <CSSTransition key={location.key} classNames={"animate"} timeout={500}>
         <Switch location={location}>
-          <Route exact path={links.create.pattern} component={CreatePage} />
-          <Route
-            exact
-            path={links.itemDetails().pattern}
-            component={ItemDetailsPage}
-          />
-          <Route exact path={links.home.pattern} component={HomePage} />
-          <Route component={ErrorPage} />
+          {routes.map((route) => (
+            <Route {...route} />
+          ))}
         </Switch>
       </CSSTransition>
     </TransitionGroup>
   );
 };
 
-export default Routes;
+export default RenderRoutes;
